4c7bba8c23fea76866d4a98985610dd101fb39fe
BatleCat <petruhin@yandex.ru>
2021-07-07 09:05:37 +0300

    заглушка на app_shdsl_task
    Заглушка: app_shdsl_task обрабатывает пакеты из очереди и пересылает их в очередь app_amak_parser_task

--- firmware/src/app_shdsl_task.c
+++ firmware/src/app_shdsl_task.c
@@ -1,8 +1,10 @@
-/*******************************************************************************
+/*------------------------------------------------------------------------------
+
   MPLAB Harmony Application Source File
 
   Company:
     Microchip Technology Inc.
+    TNG-Group
 
   File Name:
     app_shdsl_task.c
@@ -19,24 +21,35 @@
     the modules in the system or make any assumptions about when those functions
     are called.  That is the responsibility of the configuration-specific system
     files.
- *******************************************************************************/
+ ------------------------------------------------------------------------------*/
 
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Section: Included Files
-// *****************************************************************************
-// *****************************************************************************
-
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 #include "app_shdsl_task.h"
+//------------------------------------------------------------------------------
+#include <stddef.h>
+#include <string.h>
+#include "third_party/rtos/FreeRTOS/Source/include/FreeRTOS.h"
+#include "third_party/rtos/FreeRTOS/Source/include/queue.h"
+//#include "config/default/system/debug/sys_debug.h"
+#include "config/default/system/console/sys_console.h"
+#include <bsp/bsp.h>
+//------------------------------------------------------------------------------
+#include "AMAK_SHDSL_Queue.h"
+//------------------------------------------------------------------------------
+#define ENABLE_CONSOLE_MESSAGE
 
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Section: Global Data Definitions
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-// *****************************************************************************
-/* Application Data
+/*------------------------------------------------------------------------------
+   Application Data
 
   Summary:
     Holds application data
@@ -49,43 +62,59 @@
 
     Application strings and buffers are be defined outside this structure.
 */
-
+//------------------------------------------------------------------------------
 APP_SHDSL_TASK_DATA app_shdsl_taskData;
+//------------------------------------------------------------------------------
 
-// *****************************************************************************
-// *****************************************************************************
+/*------------------------------------------------------------------------------
+  Event Queue for SHDSL Task Application
+
+  Summary:
+    This variable holds event queue for SHDSL task application
+
+  Description:
+    
+  Remarks:
+    
+*/
+//------------------------------------------------------------------------------
+QueueHandle_t eventQueue_app_shdsl_task = NULL;
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Section: Application Callback Functions
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 /* TODO:  Add any necessary callback functions.
 */
 
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Section: Application Local Functions
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 
 /* TODO:  Add any necessary local functions.
 */
 
 
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-/*******************************************************************************
+/*------------------------------------------------------------------------------
   Function:
     void APP_SHDSL_TASK_Initialize ( void )
 
   Remarks:
     See prototype in app_shdsl_task.h.
  */
-
+//------------------------------------------------------------------------------
 void APP_SHDSL_TASK_Initialize ( void )
 {
     /* Place the App state machine in its initial state. */
@@ -96,17 +125,35 @@ void APP_SHDSL_TASK_Initialize ( void )
     /* TODO: Initialize your application's state machine and other
      * parameters.
      */
+    //--------------------------------------------------------------------------
+    eventQueue_app_shdsl_task = xQueueCreate( APP_SHDSL_TASK_QUEUE_LEN, EVENT_INFO_SIZE );
+    if (NULL == eventQueue_app_amak_parser_task)
+    {
+        /* Handle error condition. Not sufficient memory to create Queue */
+        #ifdef ENABLE_CONSOLE_MESSAGE
+            SYS_CONSOLE_MESSAGE(" APP_SHDSL_TASK: Handle error condition. Not sufficient memory to create Queue\r\n");
+        #endif
+    }
+    //--------------------------------------------------------------------------
+    app_shdsl_taskData.event_info.data_len = 0;
+    app_shdsl_taskData.event_info.pData = NULL;
+    app_shdsl_taskData.event_info.event_id = ENUM_EVENT_TYPE.EVENT_TYPE_UNKNOWN;
+    //--------------------------------------------------------------------------
+    app_shdsl_taskData.rx_pocket_count = 0;
+    app_shdsl_taskData.tx_pocket_count = 0;
+    //--------------------------------------------------------------------------
 }
+//------------------------------------------------------------------------------
 
 
-/******************************************************************************
+/*------------------------------------------------------------------------------
   Function:
     void APP_SHDSL_TASK_Tasks ( void )
 
   Remarks:
     See prototype in app_shdsl_task.h.
  */
-
+//------------------------------------------------------------------------------
 void APP_SHDSL_TASK_Tasks ( void )
 {
 
@@ -116,19 +163,64 @@ void APP_SHDSL_TASK_Tasks ( void )
         /* Application's initial state. */
         case APP_SHDSL_TASK_STATE_INIT:
         {
-            bool appInitialized = true;
+            bool appSHDSLTaskInitialized = false;
+            
+            if (NULL != eventQueue_app_amak_parser_task)
+            {
+                appSHDSLTaskInitialized = true;
+            }
 
-
-            if (appInitialized)
+            if (appSHDSLTaskInitialized)
             {
 
                 app_shdsl_taskData.state = APP_SHDSL_TASK_STATE_SERVICE_TASKS;
+
+                #ifdef ENABLE_CONSOLE_MESSAGE
+                    SYS_CONSOLE_MESSAGE(" APP_SHDSL_TASK: Initialization complite\r\n");
+                #endif
             }
             break;
         }
 
         case APP_SHDSL_TASK_STATE_SERVICE_TASKS:
         {
+            app_shdsl_taskData.event_info = ENUM_EVENT_TYPE.EVENT_TYPE_UNKNOWN;
+            //------------------------------------------------------------------
+            // Проверяем есть ли в очереди данные на передачу
+            //------------------------------------------------------------------
+            xQueueReceive( eventQueue_app_shdsl_task, &app_shdsl_taskData.event_info, portMAX_DELAY );
+
+            //------------------------------------------------------------------
+            // Если данные в очереди от App_AMAK_Parser_Task
+            //------------------------------------------------------------------
+            if ( ENUM_EVENT_TYPE.EVENT_TYPE_SHDSL_DATA_POCKET == app_shdsl_taskData.event_info.event_id )
+            {
+                // копирую данные из принятого события
+                pSHDSL_POCKET shdsl_pocket = malloc(SHDSL_POCKET_SIZE);
+                memset(shdsl_pocket, 0, SHDSL_POCKET_SIZE);
+                memcpy(shdsl_pocket, app_shdsl_taskData.event_info.pData, app_shdsl_taskData.event_info.data_len);
+                // освобождаю память по указателю данных из события
+                free(app_shdsl_taskData.event_info.pData);
+                
+                app_shdsl_taskData.event_info.event_id = ENUM_EVENT_TYPE.EVENT_TYPE_SHDSL_DATA_POCKET;
+                app_shdsl_taskData.event_info.data_len = SHDSL_POCKET_SIZE;
+
+                shdsl_pocket->rx_pocket_count = app_shdsl_taskData.rx_pocket_count;   // поле заполняется в app_shdsl_task, перед отправкой пакета
+
+                //shdsl_pocket->crc                   = 0;                        // расчет CRC выполняется в app_shdsl_task, перед отправкой пакета
+
+                // отправляю пакет в SHDSL-модем через SPI (DMA-операция)
+                
+                // принимаю пакет из SHDSL-модема
+                app_shdsl_taskData.rx_pocket_count++;
+                
+                // проверяю CRC пакета
+                
+                // отправляю принятый пакет в App_AMAK_Parser_Task
+                xQueueSend( eventQueue_app_amak_parser_task, (void*)&( app_shdsl_taskData.event_info ), 0 );//portMAX_DELAY);
+            }
+
+            taskYIELD();
 
             break;
         }
@@ -144,8 +236,9 @@ void APP_SHDSL_TASK_Tasks ( void )
         }
     }
 }
+//------------------------------------------------------------------------------
 
 
-/*******************************************************************************
- End of File
- */
+//------------------------------------------------------------------------------
+// End of File
+//------------------------------------------------------------------------------
